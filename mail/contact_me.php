16ebdfc2d694df13c07391067ee56fa34c199e32
David Garcia <david011994@gmail.com>
2019-12-12 01:14:33 +0100

    correccion de tildes noviembre

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'david011994@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'david.herrera@live.u-tad.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulario de contacto del sitio web:  $name";
 $email_body = "Recibiste un nuevo mensaje del formulario de contacto de tu sitio web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
 $headers = "From: david011994@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
